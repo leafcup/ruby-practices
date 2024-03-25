0307bcbe80c8e3dd7ca854eb92127bad16746ef2
leafcup <winter.triangle.for@gmail.com>
2024-03-26 00:16:00 +0900

    いただいたコメントの内容を反映2

--- 03.bowling/bowling.rb
+++ 03.bowling/bowling.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+
 score = ARGV[0]
 scores = score.split(',')
 
@@ -15,46 +16,39 @@ end
 
 frames = shots.each_slice(2).to_a
 
-# 先に10フレーム目を整理する
-# 10フレーム目は投数に関わらずframes[9]にすべて収め、合計するだけで点数を出せるようにする
-if frames.length == 12 # 3投目が存在する場合（10フレーム目の1投目と2投目が10の場合）
-  frames[-3].concat(frames[-2]).concat(frames[-1]) # 最後と最後から２番目の配列を、最後から3番目の配列に合体する
-  2.times { frames.delete_at(-1) }  # 合体済みの配列を削除する
-  frames[9].delete_at(1)  # 10フレーム目（最後の配列）の2番目の0は不要なので削除する
-elsif frames.length == 11 # 3投目が存在する場合
-  frames[-2].concat(frames[-1]) # 最後の配列を最後から2番目の配列に合体する
+while frames.length > 10
+  frames[-2].concat(frames[-1])
   frames.delete_at(-1) # 合体済みの配列を削除する
-  if frames[9][0] == 10 # 10フレーム目の2番目の数字が0の場合
-    frames[9].delete_at(1) # 不要な0を削除する
-  end
+  frames[-1].delete_at(1) if frames[-1][0] == 10
 end
 
-def strike(frame, i, frames)
-  if i <= 7 && frames[i + 1][0] == 10
-    frame.sum + 10 + frames[i + 2][0]
+
+def strike(i, next1, next2)
+  if i <= 7 && next1[0] == 10
+    10 + next2[0]
   elsif i <= 8
-    frame.sum + frames[i + 1][0] + frames[i + 1][1]
+    next1[0..1].sum
   else  # 10フレーム目
-    frame.sum
+    0
   end
 end
 
-def spare(frame, i, frames)
+def spare(i, next1)
   if i <= 8
-    frame.sum + frames[i + 1][0]
-  else
-    frame.sum  # 10フレーム目
+    next1[0]
+  else # 10フレーム目
+    0
   end
 end
 
 point = frames.each_with_index.sum do |frame, i|
-          if frame[0] == 10  # strikeの場合
-            strike(frame, i, frames)
-          elsif frame.sum == 10  # spareの場合
-            spare(frame, i, frames)
-          else  # その他の場合
-            frame.sum
-          end
-        end
-  
+  frame.sum + if frame[0] == 10  # strikeの場合
+                strike(i, frames[i + 1], frames[i + 2])
+              elsif frame.sum == 10  # spareの場合
+                spare(i, frames[i + 1])
+              else  # その他の場合
+                0
+              end
+end
+
 puts point
